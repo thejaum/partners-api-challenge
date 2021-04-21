8dee879fbeab41bb958b672e5720fe23cc83cf1c
João Castro <jaumkiss@gmail.com>
2021-04-21 11:21:11 -0300

    First approach for partner model.

---
+++ domain/src/main/java/com/thejaum/challenge/partner/model/Partner.java
@@ -0,0 +1,36 @@
+package com.thejaum.challenge.partner.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Data
+@Builder
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "partners",schema = "data")
+public class Partner {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.AUTO
+    )
+    @Column(
+            name = "id",
+            updatable = false,
+            unique = true,
+            nullable = false
+    )
+    private UUID id;
+
+    @Column(name = "trading_name",length = 255)
+    private String tradingName;
+
+    @Column(name = "owner_name",length = 255)
+    private String ownerName;
+
+    private String document;
+}
