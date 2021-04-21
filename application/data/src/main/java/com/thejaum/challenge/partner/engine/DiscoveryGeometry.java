4cde830676812b92bb98f42d05c6847f6b9379c0
João Castro <jaumkiss@gmail.com>
2021-04-21 12:33:14 -0300

    Lab working with serialization of Partner/geometry from challenge.

---
+++ application/data/src/main/java/com/thejaum/challenge/partner/engine/DiscoveryGeometry.java
@@ -0,0 +1,23 @@
+package com.thejaum.challenge.partner.engine;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thejaum.challenge.partner.dto.PartnerDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class DiscoveryGeometry {
+
+    @PostConstruct
+    void labGeoFromJson() throws IOException {
+        File file = new File("F:\\desenv\\challenges\\partners-api-challenge\\docs\\single-partners-pdv.json");
+        final PartnerDTO partnerDTO = new ObjectMapper().readValue(file, PartnerDTO.class);
+        log.info("Owner Name -> "+partnerDTO.getOwnerName());
+    }
+}
---
+++ domain/src/main/java/com/thejaum/challenge/partner/dto/PartnerDTO.java
@@ -0,0 +1,28 @@
+package com.thejaum.challenge.partner.dto;
+
+import lombok.*;
+import org.geojson.GeoJsonObject;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class PartnerDTO {
+
+    private long id;
+
+    private String tradingName;
+
+    private String ownerName;
+
+    private String document;
+
+    //coverageArea
+    private GeoJsonObject coverageArea;
+    //address
+    private GeoJsonObject address;
+
+}
